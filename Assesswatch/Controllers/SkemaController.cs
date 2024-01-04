3638fd46884dc8b341443df68f782ec9a683fd35
Ratfairy <manaotongku@gmail.com>
2024-01-04 08:53:51 +0700

    crud

---
+++ Assesswatch/Controllers/SkemaController.cs
@@ -0,0 +1,31 @@
+﻿using Assesswatch.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assesswatch.Controllers
+{
+    public class SkemaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
